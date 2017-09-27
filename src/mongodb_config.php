bacf384112ecb7430ff77aff85e82493b74cd4bd
Ukor Jidechi Ekundayo <ukorjidechi@gmail.com>
2017-09-27 01:10:31 +0100

    Added license and readme.md file

--- src/mongodb_config.php
+++ src/mongodb_config.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Created with PhpStorm by proctor.
  * @author Ukor Jidechi Ekundayo << http://ukorjidechi.com || ukorjidechi@gmail.com>>.
- * Date: 6/30/17
- * Time: 7:52 PM
+ * Date: 6/30/17, Time: 7:52 PM
  */
 
 namespace ukorJidechi\mongoDBConnector;
